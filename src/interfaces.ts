5968fad430f45d85d5789736d1ac8d9846c28599
Antonio Seprano <antonio.seprano@docebo.com>
2020-03-30 23:22:32 +0200

    Exported MessagingSystem interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -2,3 +2,4 @@ export { Message } from "./Message";
 export { IncomingMessage } from "./IncomingMessage";
 export { MessageReceiver } from "./MessageReceiver";
 export { MessageSender } from "./MessageSender";
+export { MessagingSystem } from "./MessagingSystem";
